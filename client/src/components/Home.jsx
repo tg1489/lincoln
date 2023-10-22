2c665eff1aff141a2f538c14c7b11650ed7781c7
Tony <tonyguarino1489@gmail.com>
2023-10-22 15:01:24 -0400

    Added events to events tab and removed from home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Container, Row, Col, Card, Modal, Button } from 'react-bootstrap';
 import school from '../assets/school2.png';
 import sign from '../assets/sign2.png';
-import bookfair from '../assets/bookfair.png';
-import funday from '../assets/funday.png';
 import '../Home.css';
 
 export default function Home({ isMobile }) {
@@ -38,34 +36,6 @@ export default function Home({ isMobile }) {
               simply cannot do it without you.
             </p>
           </div>
-        </Row>
-        <Row className='mt-1'>
-          <h2 className='title'>Our Latest News</h2>
-        </Row>
-        <Row className={`row-events mt-1 ${isMobile ? 'mobile-columns' : ''}`}>
-          <Col className='mb-4'>
-            {/* Add cards here */}
-            <Card key={cardKey}>
-              <Card.Img src={bookfair}></Card.Img>
-              <Card.Body>
-                <Card.Title>Book Fair</Card.Title>
-                <Card.Subtitle>February 20th-23rd, 2024</Card.Subtitle>
-                <Card.Subtitle>
-                  April (Dates TBD) (Buy 1 Get 1 Free)
-                </Card.Subtitle>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className='mb-4'>
-            <Card key={cardKey}>
-              <Card.Img src={funday}></Card.Img>
-              <Card.Body>
-                <Card.Title>Funday</Card.Title>
-                <Card.Subtitle>Dates (TBD)</Card.Subtitle>
-                <Card.Text>Click to volunteer for Funday</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
         </Row>
 
         <Row className={`membership mt-5 mb-5 ${isMobile ? 'mobile-row' : ''}`}>
