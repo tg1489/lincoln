b6a63826807cbefd62811e935a270d855e9825ff
Tony <tonyguarino1489@gmail.com>
2023-10-22 16:31:01 -0400

    added storeImages for Store tab

--- client/src/utils/storeImages.js
+++ client/src/utils/storeImages.js
@@ -1,5 +1,23 @@
-import 
-
 export const storeImages = [
-
-]
+  { src: require('../assets/store/birthdayboard.jpg') },
+  { src: require('../assets/store/chart.jpg') },
+  { src: require('../assets/store/cinchpack.png') },
+  { src: require('../assets/store/cinchpack2.png') },
+  { src: require('../assets/store/fullzipperhoodiesweatshirts.jpg') },
+  { src: require('../assets/store/hoodiesweatshirts.png') },
+  { src: require('../assets/store/lincolnpridefullzipperhoodie.jpg') },
+  { src: require('../assets/store/lincolnpridefullzipperhoodie1.jpg') },
+  { src: require('../assets/store/lincolnpridefullzipperhoodie2.jpg') },
+  { src: require('../assets/store/lincthelionplushtoy.png') },
+  { src: require('../assets/store/longsleevet-shirts.png') },
+  { src: require('../assets/store/shortsleevet-shirts.png') },
+  { src: require('../assets/store/sponsorship.png') },
+  { src: require('../assets/store/staffonlyfleecejacket.jpg') },
+  { src: require('../assets/store/staffonlypolot-shirt.jpg') },
+  { src: require('../assets/store/staffptomembership.jpg') },
+  { src: require('../assets/store/standardptomembership.png') },
+  { src: require('../assets/store/sweatpants.jpg') },
+  { src: require('../assets/store/sweatpants2.jpg') },
+  { src: require('../assets/store/tie-dyeshortsleevet-shirts.jpg') },
+  { src: require('../assets/store/tie-dye2.jpg') },
+];
