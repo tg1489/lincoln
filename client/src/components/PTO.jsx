22de08f1be44cd3dd99f474b958ccec337b0618f
Tony <tonyguarino1489@gmail.com>
2023-10-30 16:35:45 -0400

    Fixed double scrolling bar issue

--- client/src/components/PTO.jsx
+++ client/src/components/PTO.jsx
@@ -5,25 +5,23 @@ import '../styles/PTO.css';
 
 export default function PTO({ isMobile }) {
   return (
-    <Container>
-      <div className='app-container'>
-        <div className='content'>
-          <h1 className='pto-title'>
-            PTO Meeting Schedule <span className='pto-year'>2023-24</span>
-          </h1>
-          <div className='chalkboard'>
-            <Row className='line-1'>Hidden</Row>
-            <Row className='line-2'>
-              6:00 PM in Lincoln School Multipurpose Room
-            </Row>
-            <Row className='line-3'>Test</Row>
-            <Row className='line-4'>December 6th</Row>
-            <Row className='line-5'>February 22nd</Row>
-            <Row className='line-6'>Test</Row>
-            <Row className='line-7'>Additional Dates to Follow</Row>
-          </div>
+    <>
+      <Container className='app-container'>
+        <h1 className='pto-title'>
+          PTO Meeting Schedule <span className='pto-year'>2023-24</span>
+        </h1>
+        <div className={isMobile ? 'mobile-chalkboard' : 'chalkboard'}>
+          <Row className='line-1'>Hidden</Row>
+          <Row className='line-2'>
+            6:00 PM in Lincoln School Multipurpose Room
+          </Row>
+          <Row className='line-3'>Test</Row>
+          <Row className='line-4'>December 6th</Row>
+          <Row className='line-5'>February 22nd</Row>
+          <Row className='line-6'>Test</Row>
+          <Row className='line-7'>Additional Dates to Follow</Row>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </>
   );
 }
