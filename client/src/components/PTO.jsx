d9b632973a0f9f8f89f6c6e1560b4f016508c09f
Tony <tonyguarino1489@gmail.com>
2023-10-31 14:38:20 -0400

    Made the font responsive on the chalkboard

--- client/src/components/PTO.jsx
+++ client/src/components/PTO.jsx
@@ -7,19 +7,21 @@ export default function PTO({ isMobile }) {
   return (
     <>
       <Container className='app-container'>
-        <h1 className='pto-title'>
-          PTO Meeting Schedule <span className='pto-year'>2023-24</span>
-        </h1>
-        <div className={isMobile ? 'mobile-chalkboard' : 'chalkboard'}>
-          <Row className='line-1'>Hidden</Row>
-          <Row className='line-2'>
-            6:00 PM in Lincoln School Multipurpose Room
-          </Row>
-          <Row className='line-3'>Test</Row>
-          <Row className='line-4'>December 6th</Row>
-          <Row className='line-5'>February 22nd</Row>
-          <Row className='line-6'>Test</Row>
-          <Row className='line-7'>Additional Dates to Follow</Row>
+        <div>
+          <h1 className='pto-title'>
+            PTO Meeting Schedule <span className='pto-year'>2023-24</span>
+          </h1>
+          <div className={isMobile ? 'mobile-chalkboard' : 'chalkboard'}>
+            <Row className='line-1'>Hidden</Row>
+            <Row className='line-2'>
+              6:00 PM in Lincoln School Multipurpose Room
+            </Row>
+            <Row className='line-3'>Test</Row>
+            <Row className='line-4'>December 6th</Row>
+            <Row className='line-5'>February 22nd</Row>
+            <Row className='line-6'>Test</Row>
+            <Row className='line-7'>Additional Dates to Follow</Row>
+          </div>
         </div>
       </Container>
     </>
