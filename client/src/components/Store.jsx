2932749586e5157c88a45a9cbefa2bfb124a6678
Tony <tonyguarino1489@gmail.com>
2023-11-03 17:22:06 -0400

    Fixed the height of the cards to match evenly

--- client/src/components/Store.jsx
+++ client/src/components/Store.jsx
@@ -16,144 +16,114 @@ export default function Store({ isMobile }) {
   return (
     <div>
       <Container className='store-container'>
-        <Row className={`membership mt-5 mb-5 ${isMobile ? 'mobile-row' : ''}`}>
-          {isMobile ? (
-            // Mobile
-            <>
-              {/* <div className='col'>
-                <h2 className='title'>Join Us</h2>
-                <img src={school} className='membership-image mb-2' />
-              </div>
+        <Row className={`membership mt-5 mb-5`}>
+          <>
+            <Col className='myCol' md={3}>
+              <Card
+                key={cardKey}
+                className='store-card'
+                onClick={() => alert('ITS A TRAP')}
+              >
+                <Card.Img className='card-image' src={school}></Card.Img>
+                <Card.Body>
+                  <Card.Title>Membership</Card.Title>
+                  <Card.Subtitle>$10.00</Card.Subtitle>
+                  <Card.Text>Standard PTO Family Membership 2023-24</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
 
-              <div className='col'>
-                <p>
-                  Membership gives all Lincoln Elementary students in your
-                  household access to discounts for some events that the PTO
-                  sponsors for our students. Your membership fee helps the PTO
-                  fund many educational and social programs throughout the year
-                  for our children.
-                </p>
-                <p>
-                  Please complete the membership form before proceeding with
-                  your payment. Memberships require both form submission and
-                  payment. Thank you!
-                </p>
-                <div>
-                  <span className='membership-link'>Click Here</span>
-                </div>
-              </div> */}
-            </>
-          ) : (
-            // Standard
-            <>
-              <Col className='myCol' md={3}>
-                <Card
-                  key={cardKey}
-                  className='store-card'
-                  onClick={() => alert('ITS A TRAP')}
-                >
-                  <Card.Img className='card-image' src={school}></Card.Img>
-                  <Card.Body>
-                    <Card.Title>Membership</Card.Title>
-                    <Card.Subtitle>$10.00</Card.Subtitle>
-                    <Card.Text>
-                      Standard PTO Family Membership 2023-24
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
+            <Col className='myCol' md={3}>
+              <Card key={cardKey} className='store-card'>
+                <Card.Img
+                  className='card-image'
+                  src={storeImages[15].src}
+                ></Card.Img>
+                <Card.Body>
+                  <Card.Title>Staff Membership</Card.Title>
+                  <Card.Subtitle>$10.00</Card.Subtitle>
+                  <Card.Text>Staff PTO Family Membership 2023-24</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
 
-              <Col className='myCol' md={3}>
-                <Card key={cardKey} className='store-card'>
-                  <Card.Img
-                    className='card-image'
-                    src={storeImages[15].src}
-                  ></Card.Img>
-                  <Card.Body>
-                    <Card.Title>Staff Membership</Card.Title>
-                    <Card.Subtitle>$10.00</Card.Subtitle>
-                    <Card.Text>Staff PTO Family Membership 2023-24</Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
+            <Col className='myCol' md={3}>
+              <Card key={cardKey} className='store-card'>
+                <Card.Img
+                  onMouseOver={(e) =>
+                    (e.currentTarget.src = storeImages[3].src)
+                  }
+                  onMouseLeave={(e) =>
+                    (e.currentTarget.src = storeImages[2].src)
+                  }
+                  className='card-image'
+                  src={storeImages[2].src}
+                ></Card.Img>
+                <Card.Body>
+                  <Card.Title>Cinch Pack</Card.Title>
+                  <Card.Subtitle>$15.00</Card.Subtitle>
+                </Card.Body>
+              </Card>
+            </Col>
 
-              <Col className='myCol' md={3}>
-                <Card key={cardKey} className='store-card'>
-                  <Card.Img
-                    onMouseOver={(e) =>
-                      (e.currentTarget.src = storeImages[3].src)
-                    }
-                    onMouseLeave={(e) =>
-                      (e.currentTarget.src = storeImages[2].src)
-                    }
-                    className='card-image'
-                    src={storeImages[2].src}
-                  ></Card.Img>
-                  <Card.Body>
-                    <Card.Title>Cinch Pack</Card.Title>
-                    <Card.Subtitle>$15.00</Card.Subtitle>
-                  </Card.Body>
-                </Card>
-              </Col>
+            <Col className='myCol' md={3}>
+              <Card key={cardKey} className='store-card'>
+                <Card.Img
+                  onMouseOver={(e) =>
+                    (e.currentTarget.src = storeImages[8].src)
+                  }
+                  onMouseLeave={(e) =>
+                    (e.currentTarget.src = storeImages[6].src)
+                  }
+                  className='card-image'
+                  src={storeImages[6].src}
+                ></Card.Img>
+                <Card.Body>
+                  <Card.Title>Lincoln Pride Full Zipper Hoodie</Card.Title>
+                  <Card.Subtitle>$25.00</Card.Subtitle>
+                </Card.Body>
+              </Card>
+            </Col>
 
-              <Col className='myCol' md={3}>
-                <Card key={cardKey} className='store-card'>
-                  <Card.Img
-                    onMouseOver={(e) =>
-                      (e.currentTarget.src = storeImages[8].src)
-                    }
-                    onMouseLeave={(e) =>
-                      (e.currentTarget.src = storeImages[6].src)
-                    }
-                    className='card-image'
-                    src={storeImages[6].src}
-                  ></Card.Img>
-                  <Card.Body>
-                    <Card.Title>Lincoln Pride Full Zipper Hoodie</Card.Title>
-                    <Card.Subtitle>$25.00</Card.Subtitle>
-                  </Card.Body>
-                </Card>
-              </Col>
+            <Col className='myCol' md={3}>
+              <Card key={cardKey} className='store-card'>
+                <Card.Img
+                  onMouseOver={(e) =>
+                    (e.currentTarget.src = storeImages[8].src)
+                  }
+                  onMouseLeave={(e) =>
+                    (e.currentTarget.src = storeImages[7].src)
+                  }
+                  className='card-image'
+                  src={storeImages[7].src}
+                ></Card.Img>
+                <Card.Body>
+                  <Card.Title>Lincoln Pride Pull Over Hoodie</Card.Title>
+                  <Card.Subtitle>$22.00</Card.Subtitle>
+                </Card.Body>
+              </Card>
+            </Col>
 
-              <Col className='myCol' md={3}>
-                <Card key={cardKey} className='store-card'>
-                  <Card.Img
-                    onMouseOver={(e) =>
-                      (e.currentTarget.src = storeImages[8].src)
-                    }
-                    onMouseLeave={(e) =>
-                      (e.currentTarget.src = storeImages[7].src)
-                    }
-                    className='card-image'
-                    src={storeImages[7].src}
-                  ></Card.Img>
-                  <Card.Body>
-                    <Card.Title>Lincoln Pride Pull Over Hoodie</Card.Title>
-                    <Card.Subtitle>$22.00</Card.Subtitle>
-                  </Card.Body>
-                </Card>
-              </Col>
+            <Col className='myCol' md={3}>
+              <Card key={cardKey} className='store-card'>
+                <Card.Img
+                  onMouseOver={(e) =>
+                    (e.currentTarget.src = storeImages[20].src)
+                  }
+                  onMouseLeave={(e) =>
+                    (e.currentTarget.src = storeImages[19].src)
+                  }
+                  className='card-image'
+                  src={storeImages[19].src}
+                ></Card.Img>
+                <Card.Body>
+                  <Card.Title>Tie-Dye Short Sleeve Shirt</Card.Title>
+                  <Card.Subtitle>$15.00</Card.Subtitle>
+                </Card.Body>
+              </Card>
+            </Col>
 
-              <Col className='myCol' md={3}>
-                <Card key={cardKey} className='store-card'>
-                  <Card.Img
-                    onMouseOver={(e) =>
-                      (e.currentTarget.src = storeImages[20].src)
-                    }
-                    onMouseLeave={(e) =>
-                      (e.currentTarget.src = storeImages[19].src)
-                    }
-                    className='card-image'
-                    src={storeImages[19].src}
-                  ></Card.Img>
-                  <Card.Body>
-                    <Card.Title>Tie-Dye Short Sleeve Shirt</Card.Title>
-                    <Card.Subtitle>$15.00</Card.Subtitle>
-                  </Card.Body>
-                </Card>
-              </Col>
-
-              {/* <div className='col-6'>
+            {/* <div className='col-6'>
                 <p>
                   Membership gives all Lincoln Elementary students in your
                   household access to discounts for some events that the PTO
@@ -174,9 +144,10 @@ export default function Store({ isMobile }) {
                 <h2 className='title'>Join Us</h2>
                 <img src={school} className='membership-image' />
               </div> */}
-            </>
-          )}
+          </>
         </Row>
+
+        <Row></Row>
       </Container>
     </div>
   );
