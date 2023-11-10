32cb6f44bdfe41809d1030246ed11ea57e768f13
Tony <tonyguarino1489@gmail.com>
2023-11-10 13:20:57 -0500

    Fixed contact view on standard

--- client/src/components/about/Contact.jsx
+++ client/src/components/about/Contact.jsx
@@ -7,45 +7,39 @@ import location from '../../assets/about/location.png';
 export default function Contact() {
   return (
     <div className='main-container'>
-      <Row className='main-row'>
-        <Row className='contact-row'>
-          <Col className='mb-3 mt-3 contact-col'>
+      <Container>
+        <Row className='new-contact-row'>
+          <Col>
             <h3>Contact Us</h3>
           </Col>
-          <Row className='contact-row-mini mb-1'>
-            <Col md={1} className='contact-col'>
-              <img src={location} className='small-icons' />
-            </Col>
-            <Col md={1} className='location contact-col'>
-              53 Brookville Rd Edison, NJ 08817
-            </Col>
-          </Row>
-          <Row className='contact-row-mini mb-1'>
-            <Col md={1} className='contact-col'>
-              <img src={email} className='small-icons' />
-            </Col>
-            <Col md={1} className='contact-col'>
-              lncpto@gmail.com
-            </Col>
-          </Row>
-          <Row className='contact-row-mini mb-1'>
-            <Col md={1} className='contact-col'>
-              <img src={x} className='small-icons' />
-            </Col>
-            <Col md={1} className='contact-col'>
-              @pto_lincoln8
-            </Col>
-          </Row>
-          <Row className='contact-row-mini mb-1'>
-            <Col md={1} className='contact-col'>
-              <img src={facebook} className='small-icons' />
-            </Col>
-            <Col md={1} className='contact-col'>
-              @LNCPROUD
-            </Col>
-          </Row>
         </Row>
-      </Row>
+        <Row>
+          <Col md={5} className='new-contact-col mb-2'>
+            <img src={location} className='small-icons' />
+          </Col>
+          <Col className='new-contact-col2 mb-2'>
+            53 Brookville Rd Edison, NJ 08817
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5} className='new-contact-col mb-2'>
+            <img src={email} className='small-icons' />
+          </Col>
+          <Col className='new-contact-col2 mb-2'>lncpto@gmail.com</Col>
+        </Row>
+        <Row>
+          <Col md={5} className='new-contact-col mb-2'>
+            <img src={x} className='small-icons' />
+          </Col>
+          <Col className='new-contact-col2 mb-2'>@pto_lincoln8</Col>
+        </Row>
+        <Row>
+          <Col md={5} className='new-contact-col mb-2'>
+            <img src={facebook} className='small-icons' />
+          </Col>
+          <Col className='new-contact-col2 mb-2'>@LNCPROUD</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
