12a402b0bea0c72c3130afb9b3e3e2ef6cb4c11d
Tony <tonyguarino1489@gmail.com>
2023-11-10 10:40:17 -0500

    Testing before I remove bootstrap md classes

--- client/src/components/about/Contact.jsx
+++ client/src/components/about/Contact.jsx
@@ -6,38 +6,46 @@ import facebook from '../../assets/about/facebook.png';
 import location from '../../assets/about/location.png';
 export default function Contact() {
   return (
-    <>
-      <Row className='contact-row'>
-        <Col className='mb-3 mt-3'>
-          <h3>Contact Us</h3>
-        </Col>
-        <Row className='contact-row-mini mb-1'>
-          <Col md={1}>
-            <img src={location} className='small-icons' />
+    <div className='main-container'>
+      <Row className='main-row'>
+        <Row className='contact-row'>
+          <Col className='mb-3 mt-3 contact-col'>
+            <h3>Contact Us</h3>
           </Col>
-          <Col md={1} className='location'>
-            53 Brookville Rd Edison, NJ 08817
-          </Col>
-        </Row>
-        <Row className='contact-row-mini mb-1'>
-          <Col md={1}>
-            <img src={email} className='small-icons' />
-          </Col>
-          <Col md={1}>lncpto@gmail.com</Col>
-        </Row>
-        <Row className='contact-row-mini mb-1'>
-          <Col md={1}>
-            <img src={x} className='small-icons' />
-          </Col>
-          <Col md={1}>@pto_lincoln8</Col>
-        </Row>
-        <Row className='contact-row-mini mb-1'>
-          <Col md={1}>
-            <img src={facebook} className='small-icons' />
-          </Col>
-          <Col md={1}>@LNCPROUD</Col>
+          <Row className='contact-row-mini mb-1'>
+            <Col md={1} className='contact-col'>
+              <img src={location} className='small-icons' />
+            </Col>
+            <Col md={1} className='location contact-col'>
+              53 Brookville Rd Edison, NJ 08817
+            </Col>
+          </Row>
+          <Row className='contact-row-mini mb-1'>
+            <Col md={1} className='contact-col'>
+              <img src={email} className='small-icons' />
+            </Col>
+            <Col md={1} className='contact-col'>
+              lncpto@gmail.com
+            </Col>
+          </Row>
+          <Row className='contact-row-mini mb-1'>
+            <Col md={1} className='contact-col'>
+              <img src={x} className='small-icons' />
+            </Col>
+            <Col md={1} className='contact-col'>
+              @pto_lincoln8
+            </Col>
+          </Row>
+          <Row className='contact-row-mini mb-1'>
+            <Col md={1} className='contact-col'>
+              <img src={facebook} className='small-icons' />
+            </Col>
+            <Col md={1} className='contact-col'>
+              @LNCPROUD
+            </Col>
+          </Row>
         </Row>
       </Row>
-    </>
+    </div>
   );
 }
