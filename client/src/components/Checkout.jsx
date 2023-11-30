593375e55a4d568d437baefef4d9a09ef4dce650
Tony <tonyguarino1489@gmail.com>
2023-11-30 11:59:34 -0500

    Made logo show at beginning of page

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import checkoutImage from '../assets/checkout.png';
+import '../styles/Checkout.css';
 
 export default function Checkout({ isMobile, currentPage, handlePageChange }) {
   return (
-    <Container>
-      <Row>
-        <Col>
+    <Container className='checkout-view'>
+      <Row className='align-items-start'>
+        <Col className='checkout-column'>
           <img src={checkoutImage} className='checkout-image' />
-          <hr />
         </Col>
       </Row>
     </Container>
