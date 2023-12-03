f055e1e48240866d819409be4817c429cca5c0e6
Tony <tonyguarino1489@gmail.com>
2023-12-03 10:00:19 -0500

    Started seperate shopping cart component

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import checkoutImage from '../assets/checkout.png';
+import { useShoppingCart } from 'use-shopping-cart';
 import '../styles/Checkout.css';
 
 export default function Checkout({ isMobile, currentPage, handlePageChange }) {
