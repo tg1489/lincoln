b2b5349b778e35d2d01b542440e90daacf586bb7
Tony <tonyguarino1489@gmail.com>
2023-12-06 08:22:21 -0500

    Removed row

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -34,21 +34,21 @@ export default function Checkout({ isMobile, currentPage, handlePageChange }) {
         </Col>
         <Col className='checkout-card-details'>
           <h5>Card Details</h5>
-          <Row>
-            <p>Card Type</p>
-            <a href='#!' type='submit' className='text-white'>
-              <FontAwesomeIcon icon={faCcMastercard} className='fa-2x me-2' />
-            </a>
-            <a href='#!' type='submit' className='text-white'>
-              <FontAwesomeIcon icon={faCcVisa} className='fa-2x me-2' />
-            </a>
-            <a href='#!' type='submit' className='text-white'>
-              <FontAwesomeIcon icon={faCcAmex} className='fa-2x me-2' />
-            </a>
-            <a href='#!' type='submit' className='text-white'>
-              <FontAwesomeIcon icon={faCcPaypal} className='fa-2x' />
-            </a>
-          </Row>
+
+          <p>Card Type</p>
+          <a href='#!' type='submit' className='text-white'>
+            <FontAwesomeIcon icon={faCcMastercard} className='fa-2x me-2' />
+          </a>
+          <a href='#!' type='submit' className='text-white'>
+            <FontAwesomeIcon icon={faCcVisa} className='fa-2x me-2' />
+          </a>
+          <a href='#!' type='submit' className='text-white'>
+            <FontAwesomeIcon icon={faCcAmex} className='fa-2x me-2' />
+          </a>
+          <a href='#!' type='submit' className='text-white'>
+            <FontAwesomeIcon icon={faCcPaypal} className='fa-2x' />
+          </a>
+
           <form class='mt-4'>
             <div class='form-outline form-white mb-4'>
               <input
